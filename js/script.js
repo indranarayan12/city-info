2a952505cde0c1c22a47656c62a2b3157fbe3579
Indra Narayan Sah <36330980+indranarayan12@users.noreply.github.com>
2019-02-26 19:37:13 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,89 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600X400&location=' + address + '&key=AIzaSyC0xbBqWStf8Ly3k35JItX0oK7YHdzxn5w';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    // YOUR CODE GOES HERE!
+
+    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=yPO3UOeCr8RyI0vtcBUPUPUnTBbtGSi3'
+
+    $.getJSON(nytimesUrl, function( data ) {
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+        };
+
+        $(".article").error(function(){
+            alert("New York Times Articles Could Not Be Displayed")
+        });
+    }).error(function(e){
+        $nytHeaderElem.text("New York Times Articles Could Not Be Displayed");
+    });
+
+    // Wikipedia AJAX request goes here
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("failed to get wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        // jsonp: "callback"
+        success: function(response){
+            var articleList = response[1];
+
+            for(var i = 0; i < articleList.length; i++){
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        //jsonp: "callback",
+        success: function( response ) {
+            var srticleList = response[1];
+
+            for(var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url +'">' + articleStr + '</a></li>');
+            }
+        }
+    })
+
+
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
